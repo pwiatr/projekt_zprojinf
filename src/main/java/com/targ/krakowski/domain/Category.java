c0cdf8a53dee89a2e4c53e117c1b2e5bddf2e6d5
ppedziwiatr <przemyslaw.pedziwiatr@xtrf.eu>
2017-04-23 14:54:13 +0200

    Dodano encje Category

---
+++ src/main/java/com/targ/krakowski/domain/Category.java
@@ -0,0 +1,78 @@
+package com.targ.krakowski.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.springframework.data.elasticsearch.annotations.Document;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Category.
+ */
+@Entity
+@Table(name = "category")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Document(indexName = "category")
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Category name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Category category = (Category) o;
+        if (category.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            '}';
+    }
+}
---
+++ src/main/java/com/targ/krakowski/repository/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.targ.krakowski.repository;
+
+import com.targ.krakowski.domain.Category;
+
+import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the Category entity.
+ */
+@SuppressWarnings("unused")
+public interface CategoryRepository extends JpaRepository<Category,Long> {
+
+}
