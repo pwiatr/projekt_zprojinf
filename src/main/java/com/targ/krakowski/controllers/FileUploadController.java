84086c59122a45c556f30c6f6868152220d1bdab
ppedziwiatr <przemyslaw.pedziwiatr@xtrf.eu>
2017-05-21 17:37:47 +0200

    Poprawki do ofert i proby dodania formularza do uploadu

---
+++ src/main/java/com/targ/krakowski/controllers/FileUploadController.java
@@ -0,0 +1,22 @@
+package com.targ.krakowski.service;
+
+import com.targ.krakowski.storage.StorageFileNotFoundException;
+import com.targ.krakowski.storage.StorageService;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.util.stream.Collectors;
+
+@Controller
+public class FileUploadController {
+}
